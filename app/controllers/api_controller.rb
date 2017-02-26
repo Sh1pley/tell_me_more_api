b2391849bb9c0eb4d3ba2d2d5549ceed87a205df
Jesse Shipley <jesse.radack@gmail.com>
2017-02-26 15:42:54 -0700

    add nope message in json response when not authorized

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,3 +1,13 @@
 class ApiController < ApplicationController
+  before_action :authorized?
   before_action -> { doorkeeper_authorize! :api }
+
+  private
+
+    def authorized?
+      
+      unless params[:access_token]
+        render json: {message: "Nope!"}, status: 404
+      end 
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,6 @@ Rails.application.routes.draw do
       resources :users, only: [:index, :show, :destroy, :create, :update]
     end
   end
+  resources :nope, only: [:show]
 
 end
