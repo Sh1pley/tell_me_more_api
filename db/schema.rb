ee4cc1913ea1f8d3b9bd87581841c0caf2aeecca
Jesse Shipley <jesse.radack@gmail.com>
2017-02-26 17:18:00 -0700

    add ip lookup on user/sync

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224173338) do
+ActiveRecord::Schema.define(version: 20170227000241) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,6 +59,8 @@ ActiveRecord::Schema.define(version: 20170224173338) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.float    "latitude"
+    t.float    "longitude"
   end
 
   add_foreign_key "oauth_access_grants", "oauth_applications", column: "application_id"
